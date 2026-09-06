8340ecd8a7ba78e06b1b48b64ec474657c1df9d9
luis97214tic-blip <luis97214tic@gmail.com>
2026-09-06 09:13:34 -0500

    Add simple greeting and average calculation script

---
+++ notas.py
@@ -0,0 +1,17 @@
+#Saludo Simple
+print ("Hola Mundo")
+
+#Promedio de notas
+
+
+print ("Promedio de notas   ")
+
+nota1 = int(input("Ingrese la primera nota  "))
+nota2 = int(input("Ingrese la segunda nota  "))
+nota3 = int(input("Ingrese la tercera nota  "))
+
+calcular_nota = nota1 + nota2 + nota3
+calculo_final = calcular_nota /3
+
+print ("Promedio es", calculo_final)
+
